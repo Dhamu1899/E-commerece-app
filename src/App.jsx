d613fac4a08c9bba61cfcb880b29f87456e84efd
Dhamu <dhamu1899@.com>
2025-04-30 19:20:37 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
          <NavigationMenu />
 
       <Routes>
+        <Route path="/" element={<Shop />} /> 
         <Route path="/shop" element={<Shop />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/stories" element={<Stories />} />
